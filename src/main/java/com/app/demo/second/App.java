14092ea1a950e7798770c82c454b384a8ea396e8
namitha <namithagoud1212@gmail.com>
2023-11-14 21:13:11 +0530

    second

---
+++ src/main/java/com/app/demo/second/App.java
@@ -0,0 +1,13 @@
+package com.app.demo.second;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
